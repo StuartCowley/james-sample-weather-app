9116fe4a8f5310792134bb7cf69973649c0e3823
James Sample <jamesgsample@gmail.com>
2023-01-18 19:20:02 +0000

    cleaned up files and added eslint

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders weather app header', () => {
+test("renders weather app header", () => {
   render(<App />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
